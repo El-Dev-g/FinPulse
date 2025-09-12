1328e8655a6b1c992080cc57309bb5b9bca6a348
Prigid Collection <prigidcollection@gmail.com>
2025-09-12 04:23:51 +0000

    Connection Failed There was an error connecting your account. (truelayer

--- src/app/api/truelayer/callback/route.ts
+++ src/app/api/truelayer/callback/route.ts
@@ -66,7 +66,7 @@ export async function GET(request: NextRequest) {
 
   try {
     // This URI must exactly match one of the URIs configured in the Truelayer developer console.
-    const redirectUriForToken = 'https://9000-firebase-studio-1756463262326.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev/api/truelayer/callback';
+    const redirectUriForToken = `${origin}/api/truelayer/callback`;
     
     const accessToken = await exchangeCodeForToken(code, redirectUriForToken);
     
