0ad0f4b5f3fcb126b81b38533af27f091f735ae1
Prigid Collection <prigidcollection@gmail.com>
2025-09-14 12:13:41 +0000

    Error: ./src/ai/genkit.ts:4:1 [1m[31mModule not found[39m[22m: Can't

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,17 +1,10 @@
 
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase/v1';
-
-const plugins = [];
-if (process.env.GENKIT_ENV === 'dev') {
-  plugins.push(firebase());
-}
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    ...plugins,
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
